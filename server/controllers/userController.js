3a04e90875e13b4989037df199a28e95a58708f8
Benny Sufrin <bennysuf@gmail.com>
2025-07-17 15:43:29 -0400

    create routes and crud mvp for user

---
+++ server/controllers/userController.js
@@ -0,0 +1,15 @@
+const userModel = require("../models/userModel");
+
+const getUser = () => {};
+const getUsers = () => {};
+const createUser = () => {};
+const updateUser = () => {};
+const deleteUser = () => {};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getUser,
+  createUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+
+// ! dynamic route of /:id needs to be last, any req to user/* will be read as param var
+// ! page reads top to bottom
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
