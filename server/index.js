fc68e7bab233ef66e33520c740460ff18ca30b97
Benny Sufrin <bennysuf@gmail.com>
2025-07-17 15:36:25 -0400

    add routes for users and auth and make a catchall for all other routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const catchAsync = require("./utils/catchAsync");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 const { PORT, URL } = process.env;
@@ -8,11 +11,20 @@ const { PORT, URL } = process.env;
 app.use(cors(URL + PORT));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+const userRoutes = require("./routes/userRoutes");
+app.use("/users", userRoutes);
 
-// app.get("")
+const authRoutes = require("./routes/authRoutes");
+app.use("/auth", authRoutes);
+
+// catchall to throw error
+app.use(
+  catchAsync(async (req, res, next) => {
+    throw new AppError(`Can't find ${req.originalUrl} on this server`, 404);
+  })
+);
+
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
