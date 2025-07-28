4adc2c866301ecc4d2283cadf9af3b5d5c3af532
Benny Sufrin <bennysuf@gmail.com>
2025-07-28 15:12:38 -0400

    create rest of crud

--- server/controllers/assistantController.js
+++ server/controllers/assistantController.js
@@ -1,12 +1,14 @@
-const assistant = require("../db/models/assistant");
+const { assistant, user } = require("../db/models/");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 const createAssistant = catchAsync(async (req, res, next) => {
   const body = req.body;
+  const userId = req.user.id;
 
   const newAssistant = await assistant.create({
-    user_id: body.user_id,
-    apiKey: body.assistantType,
+    user_id: userId,
+    apiKey: body.apiKey,
     role: body.role,
     companyName: body.companyName,
   });
@@ -17,4 +19,93 @@ const createAssistant = catchAsync(async (req, res, next) => {
   });
 });
 
-module.exports = { createAssistant };
+const getAllAssistants = catchAsync(async (req, res, next) => {
+  const result = await assistant.findAll({
+    include: [
+      { model: user, as: "user", attributes: ["firstName", "lastName"] },
+    ],
+  });
+
+  return res.json({
+    status: "success",
+    data: result,
+  });
+});
+
+const getAssistantById = catchAsync(async (req, res, next) => {
+  const assistant_id = req.params.id;
+
+  const result = await assistant.findByPk(assistant_id, {
+    include: [
+      {
+        model: user,
+        as: "user",
+        // attributes: ["firstName", "lastName"]
+      },
+    ],
+  });
+
+  if (!result) {
+    return next(new AppError("Invalid assistant id", 400));
+  }
+
+  return res.json({
+    status: "success",
+    data: result,
+  });
+});
+
+const updateAssistant = catchAsync(async (req, res, next) => {
+  const body = req.body;
+  const userId = req.user.id;
+  const assistant_id = req.params.id;
+
+  const result = await assistant.findOne({
+    where: { id: assistant_id },
+    user_id: userId,
+  });
+
+  if (!result) {
+    return next(new AppError("Invalid assistant id", 400));
+  }
+
+  result.apiKey = body.apiKey;
+  result.role = body.role;
+  result.companyName = body.companyName;
+
+  const updatedResult = await result.save();
+
+  return res.json({
+    status: "success",
+    data: updatedResult,
+  });
+});
+
+const deleteAssistant = catchAsync(async (req, res, next) => {
+  const userId = req.user.id;
+  const assistant_id = req.params.id;
+
+  const result = await assistant.findOne({
+    where: { id: assistant_id },
+    user_id: userId,
+  });
+
+  if (!result) {
+    return next(new AppError("Invalid assistant id", 400));
+  }
+
+  const updatedResult = await result.destroy();
+
+  return res.json({
+    status: "success",
+    message: "Record deleted successfully",
+  });
+});
+
+module.exports = {
+  createAssistant,
+  getAllAssistants,
+  getAssistantById,
+  updateAssistant,
+  deleteAssistant,
+};
