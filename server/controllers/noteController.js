b0535f601cd6f3c7b6ef950b268807cdf2afec92
Benny Sufrin <bennysuf@gmail.com>
2025-07-29 13:04:41 -0400

    add rest of crud

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -1,11 +1,13 @@
-const {note} = require("../db/models");
+const { note, assistant } = require("../db/models");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 const createNote = catchAsync(async (req, res, next) => {
   const body = req.body;
+  const assistantId = req.params.assistantId;
 
   const newNote = await note.create({
-    assistant_id: body.assistant_id,
+    assistant_id: assistantId,
     noteName: body.noteName,
     noteText: body.noteText,
   });
@@ -15,5 +17,115 @@ const createNote = catchAsync(async (req, res, next) => {
     data: newNote,
   });
 });
+const getAllNotes = catchAsync(async (req, res, next) => {
+  console.log("made to note req")
+  const result = await note.findAll(
+    // {
+    // include: [{ model: assistant, as: "assistant" }],
+  // }
+);
 
-module.exports = { createNote };
+  console.log("result for note", result)
+
+  return res.json({
+    status: "success",
+    data: result,
+  });
+});
+
+const getNoteById = catchAsync(async (req, res, next) => {
+  const { assistantId, noteId } = req.params;
+
+  const result = await note.findOne({
+    where: {
+      id: noteId,
+      assistant_id: assistantId,
+    },
+    include: {
+      model: assistant,
+      as: "assistant",
+      where: {
+        user_id: req.user.id,
+      },
+    },
+  });
+
+  if (!result) {
+    return next(new AppError("Note not found or access denied", 403));
+  }
+
+  return res.json({
+    status: "success",
+    data: result,
+  });
+});
+
+const updateNote = catchAsync(async (req, res, next) => {
+  const { assistantId, noteId } = req.params;
+  const body = req.body;
+
+  const result = await note.findOne({
+    where: {
+      id: noteId,
+      assistant_id: assistantId,
+    },
+    include: {
+      model: assistant,
+      as: "assistant",
+      where: {
+        user_id: req.user.id,
+      },
+    },
+  });
+
+  if (!result) {
+    return next(new AppError("Note not found or access denied", 403));
+  }
+
+  result.noteName = body.noteName;
+  result.noteText = body.noteText;
+
+  const updatedResult = await result.save();
+
+  return res.json({
+    status: "success",
+    data: updatedResult,
+  });
+});
+
+const deleteNote = catchAsync(async (req, res, next) => {
+  const { assistantId, noteId } = req.params;
+
+  const result = await note.findOne({
+    where: {
+      id: noteId,
+      assistant_id: assistantId,
+    },
+    include: {
+      model: assistant,
+      as: "assistant",
+      where: {
+        user_id: req.user.id,
+      },
+    },
+  });
+
+  if (!result) {
+    return next(new AppError("Note not found or access denied", 403));
+  }
+
+  await result.destroy();
+
+  return res.json({
+    status: "success",
+    message: "Record deleted successfully",
+  });
+});
+
+module.exports = {
+  createNote,
+  getAllNotes,
+  getNoteById,
+  updateNote,
+  deleteNote,
+};
--- server/controllers/promptController.js
+++ server/controllers/promptController.js
@@ -1,11 +1,13 @@
-const {prompt} = require("../db/models");
+const { prompt, assistant } = require("../db/models");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 const createPrompt = catchAsync(async (req, res, next) => {
   const body = req.body;
+  const assistantId = req.params.assistantId;
 
   const newPrompt = await prompt.create({
-    assistant_id: body.assistant_id,
+    assistant_id: assistantId,
     promptType: body.promptType,
     promptText: body.promptText,
   });
@@ -16,4 +18,110 @@ const createPrompt = catchAsync(async (req, res, next) => {
   });
 });
 
-module.exports = { createPrompt };
+const getAllPrompts = catchAsync(async (req, res, next) => {
+  const result = await prompt.findAll({
+    include: [{ model: assistant, as: "assistant" }],
+  });
+
+  return res.json({
+    status: "success",
+    data: result,
+  });
+});
+
+const getPromptById = catchAsync(async (req, res, next) => {
+  const { assistantId, promptId } = req.params;
+
+  const result = await prompt.findOne({
+    where: {
+      id: promptId,
+      assistant_id: assistantId,
+    },
+    include: {
+      model: assistant,
+      as: "assistant",
+      where: {
+        user_id: req.user.id,
+      },
+    },
+  });
+
+  if (!result) {
+    return next(new AppError("Prompt not found or access denied", 403));
+  }
+
+  return res.json({
+    status: "success",
+    data: result,
+  });
+});
+
+const updatePrompt = catchAsync(async (req, res, next) => {
+  const { assistantId, promptId } = req.params;
+  const body = req.body;
+
+  const result = await prompt.findOne({
+    where: {
+      id: promptId,
+      assistant_id: assistantId,
+    },
+    include: {
+      model: assistant,
+      as: "assistant",
+      where: {
+        user_id: req.user.id,
+      },
+    },
+  });
+
+  if (!result) {
+    return next(new AppError("Prompt not found or access denied", 403));
+  }
+
+  result.promptText = body.promptText;
+  result.promptType = body.promptType;
+
+  const updatedResult = await result.save();
+
+  return res.json({
+    status: "success",
+    data: updatedResult,
+  });
+});
+
+const deletePrompt = catchAsync(async (req, res, next) => {
+  const { assistantId, promptId } = req.params;
+
+  const result = await prompt.findOne({
+    where: {
+      id: promptId,
+      assistant_id: assistantId,
+    },
+    include: {
+      model: assistant,
+      as: "assistant",
+      where: {
+        user_id: req.user.id,
+      },
+    },
+  });
+
+  if (!result) {
+    return next(new AppError("Prompt not found or access denied", 403));
+  }
+
+  await result.destroy();
+
+  return res.json({
+    status: "success",
+    message: "Record deleted successfully",
+  });
+});
+
+module.exports = {
+  createPrompt,
+  getAllPrompts,
+  getPromptById,
+  updatePrompt,
+  deletePrompt,
+};
