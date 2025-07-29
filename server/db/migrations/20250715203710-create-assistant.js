d4813914cb9d2196183078b92748cfbde6783b42
Benny Sufrin <bennysuf@gmail.com>
2025-07-29 13:05:54 -0400

    change allowNull back to false

--- server/db/migrations/20250715203710-create-assistant.js
+++ server/db/migrations/20250715203710-create-assistant.js
@@ -17,7 +17,7 @@ module.exports = {
       },
       user_id: {
         type: Sequelize.INTEGER,
-        allowNull: true,
+        allowNull: false,
         references: {
           model: 'users',
           key: 'id',
