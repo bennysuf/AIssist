3dc17e616688b4ddf63a897ab8a9a7d524ad10bd
Benny Sufrin <bennysuf@gmail.com>
2025-08-05 17:00:50 -0400

    add logout route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,11 @@
-const { signup, login } = require("../controllers/authController");
+const { signup, login, logout } = require("../controllers/authController");
 
 const router = require("express").Router();
 
 router.route("/signup").post(signup);
 
 router.route("/login").post(login)
+
+router.route("/logout").post(logout)
 
 module.exports = router;
