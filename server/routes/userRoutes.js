5b2660de8c29dae9d32d34aea8f23d2341286204
Benny Sufrin <bennysuf@gmail.com>
2025-08-05 17:01:20 -0400

    update and add routes, add authentication

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,18 +2,24 @@ const express = require("express");
 const router = express.Router();
 const {
   getUser,
-  createUser,
   getUsers,
   updateUser,
   deleteUser,
+  getUserById,
 } = require("../controllers/userController");
+const { authentication } = require("../controllers/authController");
 
 // ! dynamic route of /:id needs to be last, any req to user/* will be read as param var
 // ! page reads top to bottom
 
-router.route("/").get(getUsers)
-// .post(createUser);
+router.route("/").get(authentication, getUser);
 
-router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+router.route("/all").get(authentication, getUsers);
+
+router
+  .route("/:userId")
+  .get(authentication, getUserById)
+  .put(authentication, updateUser)
+  .delete(authentication, deleteUser);
 
 module.exports = router;
