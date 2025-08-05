b1f67430c314713eea945a3cab59db3047dabd4f
Benny Sufrin <bennysuf@gmail.com>
2025-08-05 16:50:38 -0400

    create mvp layout for component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import "./App.css";
 import AppRoutes from "./AppRoutes";
+import { useUserStore } from "./util/stores/userStore";
+import NavBar from "./components/NavBar";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const fetchUser = useUserStore((state) => state.fetchUser);
+  const location = useLocation();
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return (
-    <>
+    <div>
+      {!location.pathname.includes("login") && (
+        <div style={{ display: "flex", height: "100vh" }}>
+          <NavBar />
+        </div>
+      )}
+      {/* <div style={{ flex: 1, padding: "1rem", overflowY: "auto" }}> */}
       <AppRoutes />
-    </>
+      {/* </div> */}
+    </div>
   );
 }
 
