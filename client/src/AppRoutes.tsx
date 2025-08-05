a973ec48dba7e948cb200545635498b0005df5a7
Benny Sufrin <bennysuf@gmail.com>
2025-08-05 16:51:17 -0400

    set up routing

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Portal from './pages/Portal';
+import { Routes, Route } from "react-router-dom";
+import Portal from "./pages/Portal";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 
 const AppRoutes = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/portal" element={<Portal />} />
-    </Routes>
-  </BrowserRouter>
+  <Routes>
+    <Route path="/portal" element={<Portal />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/" element={<Home/>}/>
+  </Routes>
 );
 
 export default AppRoutes;
