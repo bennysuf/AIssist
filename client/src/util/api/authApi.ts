cc3bbc0530c26244ffd5638e5a9cb3639f61bb72
Benny Sufrin <bennysuf@gmail.com>
2025-08-10 17:51:19 -0400

    update and clean up signup method

--- client/src/util/api/authApi.ts
+++ client/src/util/api/authApi.ts
@@ -1,19 +1,16 @@
 import axios from "./axios";
-import type { User } from "../dto/userDtos";
+import type { SignupUser } from "../dto/userDtos";
 
 export async function login(email: string, password: string): Promise<number> {
-    const  {status}  = await axios.post("/auth/login", { email, password });
-    return status;
-  }
-  
-  export async function signup(
-    user: User, password: string, passwordConfirmation: string
-  ): Promise<User> {
-    // ! test params in backend
-    const { data } = await axios.post("/auth/signup", { user, password, passwordConfirmation });
-    return data.message;
-  }
+  const { status } = await axios.post("/auth/login", { email, password });
+  return status;
+}
 
-  export async function logout(): Promise<void> {
-    await axios.post("/auth/logout");
-  }
+export async function signup(user: SignupUser): Promise<number> {
+  const { status } = await axios.post("/auth/signup", { user });
+  return status;
+}
+
+export async function logout(): Promise<void> {
+  await axios.post("/auth/logout");
+}
