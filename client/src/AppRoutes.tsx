49b6d52b002d2e9b984cce5479b18c49ce157a07
Benny Sufrin <bennysuf@gmail.com>
2025-08-14 23:09:07 -0400

    add inbox route

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Portal from "./pages/Portal";
 import Home from "./pages/Home";
 import Authentication from "./pages/Authentication";
+import Inbox from "./pages/Inbox";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/portal" element={<Portal />} />
     <Route path="/auth/*" element={<Authentication />} />
+    <Route path="/inbox" element={<Inbox />} />
     <Route path="/" element={<Home />} />
   </Routes>
 );
