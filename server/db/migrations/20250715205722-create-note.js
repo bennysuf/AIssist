06134d5415397c1c1200d7bb1c08421ee1a53ab2
Benny Sufrin <bennysuf@gmail.com>
2025-08-14 22:58:55 -0400

    add attributes, default values, and change single quotes to double

--- server/db/migrations/20250715205722-create-note.js
+++ server/db/migrations/20250715205722-create-note.js
@@ -1,41 +1,51 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('notes', {
+    await queryInterface.createTable("notes", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       assistant_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'assistants',
-          key: 'id',
+          model: "assistants",
+          key: "id",
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
-      noteName: {
-        type: Sequelize.STRING
+      callerName: {
+        type: Sequelize.STRING,
+        defaultValue: "",
       },
       noteText: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        defaultValue: "",
+      },
+      noteSummery: {
+        type: Sequelize.STRING,
+        defaultValue: "",
+      },
+      markedRead: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('notes');
-  }
+    await queryInterface.dropTable("notes");
+  },
 };
