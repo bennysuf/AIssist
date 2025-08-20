98809570f62b632b640098957b58b7e931875af0
Benny Sufrin <bennysuf@gmail.com>
2025-08-20 16:38:39 -0400

    update route and send data for update note method:

--- client/src/util/api/noteApi.ts
+++ client/src/util/api/noteApi.ts
@@ -6,9 +6,6 @@ export async function getNotes(
   filter: string,
   opts?: { before?: number }
 ): Promise<Note[]> {
-//   console.log(
-//     `notes api: assistant: ${assistantId}, filter: ${filter}, opts: ${opts}`
-//   );
   const { data } = await axios.get("/notes/load_notes/", {
     params: {
       assistantId,
@@ -21,6 +18,10 @@ export async function getNotes(
 }
 
 export async function updateNote(updates: Partial<Note>): Promise<Note> {
-  const { data } = await axios.patch("/", updates);
-  return data;
+  const {id, assistant_id, markedRead} = updates
+  const { data } = await axios.patch(`/notes/${id}`, {
+    assistantId: assistant_id,
+    markedRead: markedRead
+  });
+  return data.data;
 }
