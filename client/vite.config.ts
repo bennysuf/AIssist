d879d7c62815467e338be9ca5704cd928dfacd2e
Benny Sufrin <bennysuf@gmail.com>
2025-09-04 17:33:36 -0400

    add config for vsCode debugging

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    sourcemap: true // <-- this is required for source maps in build mode
+  }
 })
