52f50782e5ab5385fed5719de6f78bcc7890a96f
Benny Sufrin <bennysuf@gmail.com>
2025-09-04 17:33:58 -0400

    add route for profile page

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -3,12 +3,14 @@ import Portal from "./pages/Portal";
 import Home from "./pages/Home";
 import Authentication from "./pages/Authentication";
 import Inbox from "./pages/Inbox";
+import Profile from "./pages/Profile";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/portal" element={<Portal />} />
     <Route path="/auth/*" element={<Authentication />} />
     <Route path="/inbox" element={<Inbox />} />
+    <Route path="/profile" element={<Profile />} />
     <Route path="/" element={<Home />} />
   </Routes>
 );
