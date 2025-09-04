f4b11a863fa55b17982bd763cee62176814fb8cc
Benny Sufrin <bennysuf@gmail.com>
2025-09-04 17:39:00 -0400

    create logic for updating user info

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const { user, assistant, prompt } = require("../db/models");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 const getUser = catchAsync(async (req, res, next) => {
@@ -60,6 +61,52 @@ const getUserById = catchAsync(async (req, res, next) => {
 
 const updateUser = catchAsync(async (req, res, next) => {
   const body = req.body;
+  const userId = req.user.id;
+
+  const { firstName, lastName, email, phone } = body;
+
+  const result = await user.findOne({
+    where: { id: userId },
+    attributes: { exclude: ["password"] },
+    include: [
+      {
+        model: assistant,
+        as: "assistants",
+        attributes: { exclude: ["createdAt", "updatedAt", "user_id"] },
+        include: [
+          {
+            model: prompt,
+            as: "prompts",
+            attributes: { exclude: ["createdAt", "updatedAt", "assistant_id"] },
+          },
+        ],
+      },
+    ],
+  });
+
+  if (!result) {
+    return next(new AppError("User not found or access denied", 403));
+  }
+
+  result.firstName = firstName;
+  result.lastName = lastName;
+  result.email = email;
+  result.phone = phone;
+
+  const updatedResult = await result.save();
+
+  // return limited updated user data
+  return res.json({
+    status: "success",
+    data: {
+      id: result.id,
+      firstName: updatedResult.firstName,
+      lastName: updatedResult.lastName,
+      email: updatedResult.email,
+      phone: updatedResult.phone,
+      assistants: result.assistants,
+    },
+  });
 });
 
 const deleteUser = catchAsync(async (req, res, next) => {
